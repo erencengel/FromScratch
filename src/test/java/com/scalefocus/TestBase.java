966b3e75381fba48e70a9b09ed8c36002afc9a9f
Eren ÇENGEL <erencengel@gmail.com>
2021-11-12 22:20:07 +0300

    second commit

--- src/test/java/com/scalefocus/TestBase.java
+++ src/test/java/com/scalefocus/TestBase.java
@@ -2,6 +2,7 @@ package com.scalefocus;
 
 import com.utilities.WebDriverInstance;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -10,12 +11,14 @@ import java.util.concurrent.TimeUnit;
 public class TestBase {
 
     protected WebDriver driver;
+    protected WebDriverWait webDriverWait;
 
     @BeforeMethod
     public void setUp(){
         driver = WebDriverInstance.getDriver("chrome-headless");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        webDriverWait = new WebDriverWait(driver,10);
     }
 
     @AfterMethod
